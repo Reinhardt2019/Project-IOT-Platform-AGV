9114ca069f4cc2639555378c59b8598dd22517d8
Molly <49314691+MollySunqy@users.noreply.github.com>
2022-01-13 11:13:56 +0800

    Add files via upload

---
+++ 03 Tools/03 Software/01 ROS2/00 Demo_Turtlesim/src/my_package/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'my_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='molly',
+    maintainer_email='qingyi.sun.ext@siemens.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'monitor = my_package.monitor:main',
+            'place = my_package.placeturtle:main'
+        ],
+    },
+)
