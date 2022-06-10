e2f8b012e6723c9063887348fe342948d0dfa94b
week0425 <yeshuangxiu@qq.com>
2022-06-10 11:45:05 +0800

    hand gesture recognition source code

---
+++ documents/Phase 02-Output 04-AGV Motion Control by AI Vision/01 Implement Docs/02 gesture_recognition/README.md
@@ -0,0 +1,36 @@
+# 手势识别数据采集与训练
+## 数据采集
+我们定义了6种手势，分别是`start`、`end`、`turn_left`、`turn_right`、`go_forward`、`go_backward`。运行`get_value.py`采集数据，每个手势采集30组，每组包含30帧双手关节坐标数据,存储在同目录下的MP_Data文件夹中。
+其中，手势定义代码为
+```python
+actions = np.array(['start', 'end', 'turn_left','turn_right', 'go_forward', 'go_backward'])
+```
+## 模型训练
+使用Jupyter依次运行`train.ipynb`中的代码块，LSTM深度学习网络结构定义代码为
+```python
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,126)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu',kernel_regularizer=regularizers.l2(0.01),
+                activity_regularizer=regularizers.l1(0.01)))
+model.add(Dense(32, activation='relu',kernel_regularizer=regularizers.l2(0.01),
+                activity_regularizer=regularizers.l1(0.01)))
+model.add(Dense(actions.shape[0], activation='softmax'))
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+```
+LSTM深度学习网络结构如下图
+![图片](https://github.com/week0425/Project-IOT-Platform-AGV/blob/58243f2c9fb5cde6c888a003f8a191eb268b03b3/documents/Phase%2002-Output%2004-AGV%20Motion%20Control%20by%20AI%20Vision/02%20gesture_recognition/train_result/LSTM_network_structure.PNG)
+
+训练代码为
+```python
+model.fit(X_train, y_train, epochs=2000, validation_data=(X_test, y_test), callbacks=[tb_callback], shuffle=True)
+```
+在训练过程中观察输出结果，当训练集和测试集的识别准确率都达到1时（如下图所示），及时停止训练，防止过拟合。如果训练集准确率达到1，而测试集准确率始终到不了1，可以重新再训练，直到满足要求。
+![图片](https://github.com/week0425/Project-IOT-Platform-AGV/blob/58243f2c9fb5cde6c888a003f8a191eb268b03b3/documents/Phase%2002-Output%2004-AGV%20Motion%20Control%20by%20AI%20Vision/02%20gesture_recognition/train_result/train_result.PNG)
+![图片](https://github.com/week0425/Project-IOT-Platform-AGV/blob/58243f2c9fb5cde6c888a003f8a191eb268b03b3/documents/Phase%2002-Output%2004-AGV%20Motion%20Control%20by%20AI%20Vision/02%20gesture_recognition/train_result/loss.png)
+![图片](https://github.com/week0425/Project-IOT-Platform-AGV/blob/58243f2c9fb5cde6c888a003f8a191eb268b03b3/documents/Phase%2002-Output%2004-AGV%20Motion%20Control%20by%20AI%20Vision/02%20gesture_recognition/train_result/accuracy.png)
+
+将训练权重保存，命名为`action.h5`。
+## 手势识别效果测试
+运行`identify.py`测试识别效果。
---
+++ documents/Phase 02-Output 04-AGV Motion Control by AI Vision/01 Implement Docs/02 gesture_recognition/get_value.py
@@ -0,0 +1,106 @@
+import cv2
+import numpy as np
+import os
+import mediapipe as mp
+
+mp_holistic = mp.solutions.holistic # Holistic model
+mp_drawing = mp.solutions.drawing_utils # Drawing utilities
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
+    image.flags.writeable = False                  # Image is no longer writeable
+    results = model.process(image)                 # Make prediction
+    image.flags.writeable = True                   # Image is now writeable
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
+    return image, results
+
+def draw_styled_landmarks(image, results):
+    """
+    要训练脸部坐标就取消注释
+    # Draw face connections
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS,
+                             mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
+                             mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
+                             )
+    
+    # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
+                             mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
+                             )
+    """
+    # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
+                             mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
+                             )
+    # Draw right hand connections
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
+                             mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                             )
+
+def extract_keypoints(results):
+    #pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
+    #face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    #return np.concatenate([pose, lh, rh])
+    return np.concatenate([lh,rh])
+
+# Path for exported data, numpy arrays
+DATA_PATH = os.path.join('MP_Data2')
+
+# Actions that we try to detect
+actions = np.array(['start', 'end', 'turn_left','turn_right', 'go_forward', 'go_backward'])
+# Thirty videos worth of data
+no_sequences = 120
+# Videos are going to be 30 frames in length
+sequence_length = 15
+for action in actions:
+    for sequence in range(no_sequences):
+        try:
+            os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
+        except:
+            pass
+
+cap = cv2.VideoCapture(0)
+# Set mediapipe model
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    # NEW LOOP
+    # Loop through actions
+    for action in actions:
+        # Loop through sequences aka videos
+        for sequence in range(no_sequences):
+            # Loop through video length aka sequence length
+            for frame_num in range(sequence_length):
+                # Read feed
+                ret, frame = cap.read()
+                # Make detections
+                image, results = mediapipe_detection(frame, holistic)
+                #                 print(results)
+                # Draw landmarks
+                draw_styled_landmarks(image, results)
+                # NEW Apply wait logic
+                if frame_num == 0:
+                    cv2.putText(image, 'STARTING COLLECTION', (120, 200),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 4, cv2.LINE_AA)
+                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15, 12),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                    # Show to screen
+                    cv2.imshow('OpenCV Feed', image)
+                    cv2.waitKey(2000)
+                else:
+                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15, 12),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                    # Show to screen
+                    cv2.imshow('OpenCV Feed', image)
+                # NEW Export keypoints
+                keypoints = extract_keypoints(results)
+                npy_path = os.path.join(DATA_PATH, action, str(sequence), str(frame_num))
+                np.save(npy_path, keypoints)
+                # Break gracefully
+                if cv2.waitKey(10) & 0xFF == ord('q'):
+                    break
+cap.release()
+cv2.destroyAllWindows()
