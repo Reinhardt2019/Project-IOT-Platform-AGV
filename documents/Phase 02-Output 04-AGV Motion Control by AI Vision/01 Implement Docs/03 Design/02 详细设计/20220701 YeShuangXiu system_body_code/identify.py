c441584f1d271449cc9e3da4c6730a98ff01880e
week0425 <71442861+week0425@users.noreply.github.com>
2022-08-03 16:58:16 +0800

    code about gesture identify

---
+++ documents/Phase 02-Output 04-AGV Motion Control by AI Vision/01 Implement Docs/03 Design/02 详细设计/20220701 YeShuangXiu system_body_code/identify.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# coding:utf-8
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+import cv2
+import numpy as np
+import mediapipe as mp
+from tensorflow.keras import regularizers
+
+mp_holistic = mp.solutions.holistic # Holistic model
+mp_drawing = mp.solutions.drawing_utils # Drawing utilities
+
+sequence = []
+sentence = 'None'
+predictions = []
+threshold = 0.95
+actions = np.array(['start', 'end', 'turn_left','turn_right', 'go_forward', 'go_backward'])
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
+    image.flags.writeable = False                  # Image is no longer writeable
+    results = model.process(image)                 # Make prediction
+    image.flags.writeable = True                   # Image is now writeable
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
+    return image, results
+
+def draw_styled_landmarks(image, results):
+    # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
+                             mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
+                             )
+    # Draw right hand connections
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
+                             mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                             )
+
+def extract_keypoints(results):
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([lh, rh])
+
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(5,126)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu',kernel_regularizer=regularizers.l2(0.01),
+                activity_regularizer=regularizers.l1(0.01)))
+model.add(Dense(32, activation='relu',kernel_regularizer=regularizers.l2(0.01),
+                activity_regularizer=regularizers.l1(0.01)))
+model.add(Dense(actions.shape[0], activation='softmax'))
+model.load_weights('./src/get_image/scripts/actionf.h5')
+
+colors = [(245, 117, 16), (117, 245, 16), (16, 117, 245),(16, 11, 245),(16, 117, 24),(1, 117, 245)]
+
+def prob_viz(res, actions, input_frame, colors):
+    output_frame = input_frame.copy()
+    for num, prob in enumerate(res):
+        cv2.rectangle(output_frame, (0, 60 + num * 40), (int(prob * 100), 90 + num * 40), colors[num], -1)
+        cv2.putText(output_frame, actions[num], (0, 85 + num * 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,
+                    cv2.LINE_AA)
+    return output_frame
+
+cap = cv2.VideoCapture(0)
+# Set mediapipe model
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+        # Read feed
+        ret, frame = cap.read()
+        # Make detections
+        image, results = mediapipe_detection(frame, holistic)
+        print(results)
+        # Draw landmarks
+        draw_styled_landmarks(image, results)
+        # 2. Prediction logic
+        keypoints = extract_keypoints(results)
+        if not keypoints.any() == 0:
+            sequence.append(keypoints)
+        else:
+            equence = []
+            sentence = 'None'
+        sequence = sequence[-5:]
+        if len(sequence) == 5:
+            res = model.predict(np.expand_dims(sequence, axis=0))[0]
+            print(actions[np.argmax(res)])
+            predictions.append(np.argmax(res))
+            # 3. Viz logic
+            if np.unique(predictions[-3:])[0]==np.argmax(res): 
+                if res[np.argmax(res)] > threshold:
+                    sentence =  actions[np.argmax(res)]
+                else:
+                    sentence = 'None'
+            # Viz probabilities
+            #image = prob_viz(res, actions, image, colors)
+        cv2.rectangle(image, (0, 0), (640, 40), (245, 117, 16), -1)
+        cv2.putText(image, sentence, (3, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        
+        # Show to screen
+        cv2.imshow('OpenCV Feed', image)
+        # Break gracefully
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+cap.release()
+cv2.destroyAllWindows()
